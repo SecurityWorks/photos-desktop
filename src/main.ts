6f9aab17db3f62f05b0b59445b62bbc4e3c9dfdf
Abhinav <abhinavk.grd@gmail.com>
2023-12-27 13:05:22 +0530

    update to use when ready instead on ready event

--- src/main.ts
+++ src/main.ts
@@ -80,7 +80,7 @@ if (!gotTheLock) {
     // This method will be called when Electron has finished
     // initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
-    app.on('ready', async () => {
+    app.whenReady().then(async () => {
         logSystemInfo();
         setupMainProcessStatsLogger();
         const hasOptedOutOfCrashReports = getOptOutOfCrashReports();
